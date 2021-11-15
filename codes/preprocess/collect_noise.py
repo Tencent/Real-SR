ba8611213bef4067ee165a8f65de7ee0b6965722
lucas.a.alcantara <lucas.a.alcantara@accenture.com>
2021-11-15 20:18:32 -0300

    fix missing yaml loader

--- codes/preprocess/collect_noise.py
+++ codes/preprocess/collect_noise.py
@@ -15,7 +15,7 @@ opt = parser.parse_args()
 
 # define input and target directories
 with open('./preprocess/paths.yml', 'r') as stream:
-    PATHS = yaml.load(stream)
+    PATHS = yaml.load(stream, Loader=yaml.SafeLoader)
 
 
 def noise_patch(rgb_img, sp, max_var, min_mean):
